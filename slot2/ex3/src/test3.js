dff19a93c94d6a6832935b79a399f6dc4fce4e80
jijijijijibao14 <de180823nguyenquocbao@gmail.com>
2025-09-17 09:18:31 +0700

    add slot3

---
+++ slot2/ex3/src/test3.js
@@ -0,0 +1,15 @@
+function example(){
+    let x = 10;
+
+    if(true){
+        let y=20;
+        console.log(x);
+        console.log(y);
+    }
+
+    console.log(x);
+    console.log(y);
+}
+example();
+
+

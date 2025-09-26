aab6ab01e558e0c173bc6fae75ffe06ccb16a591
jijijijijibao14 <de180823nguyenquocbao@gmail.com>
2025-09-26 10:08:34 +0700

    add ex

--- slot5/ex2/src/App.js
+++ slot5/ex2/src/App.js
@@ -1,7 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import { Exercise1 } from './components/exercise1';
-import { Exercise2 } from './components/exercise2';
 import { Exercise6 } from '../../ex2/src/components/exercise6';
 import { Exercise7 } from '../../ex2/src/components/exercise7';
 import { Exercise8 } from '../../ex2/src/components/exercise8';
@@ -9,8 +7,6 @@ import { Exercise8 } from '../../ex2/src/components/exercise8';
 function App() {
   return (
     <div className="App">
-      <Exercise1 />
-      <Exercise2 />
       <Exercise6 />
       <Exercise7 />
       <Exercise8 />
